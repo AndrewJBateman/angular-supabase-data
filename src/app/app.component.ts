8eb79a48c4e07658988d0c347fe63208ee71bbd3
AndrewJBateman <gomezbateman@yahoo.com>
2021-07-02 13:51:57 +0200

    refactor: template & database functions updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from './services/api.service';
+import { DatabaseService } from './services/database.service';
 import { Todo } from './models/todo.model';
 
 @Component({
@@ -8,19 +8,16 @@ import { Todo } from './models/todo.model';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  todos: Todo[];
-  todo: Todo;
-  // todos: Todo[] = [{id: '', name: '', done: false}] as Todo[];
-  // todo: Todo = {id: '', name: '', done: false} as Todo;
+  todos: Todo[] = [{id: '', name: '', done: false}] as Todo[];
+  todo: Todo = {id: '', name: '', done: false} as Todo;
   actionLabel: string;
 
-  constructor(private api: ApiService) {}
+  constructor(private dbService: DatabaseService) {}
 
   ngOnInit() {
     this.clear();
-
-    this.api.getTodos().then((payload) => {
-      this.todos = payload.data;
+    this.dbService.getTodos().then((payload) => {
+        this.todos = payload.data;
     });
   }
 
@@ -29,41 +26,50 @@ export class AppComponent implements OnInit {
     this.actionLabel = 'ADD';
   }
 
-  addTodo() {
+  onClearInput() {
+      console.log('clear input clicked');
+      this.todo = new Todo();
+  }
+
+  onAddTodo() {
     if (this.todo.id) {
-      this.update();
+      this.updateTodo();
       return;
     }
-    this.api.addTodo(this.todo).then((payload) => {
+    this.dbService.addTodo(this.todo).then(
+      (payload) => {
       this.todos.push(payload.data[0]);
       this.clear();
     });
   }
 
-  select(todo: Todo) {
+  onSelectTodo(todo: Todo) {
     this.todo = todo;
     this.actionLabel = 'UPDATE';
   }
 
-  update() {
-    this.api.update(this.todo).then(() => {
-      let index = this.todos.findIndex((t) => t.id == this.todo.id);
+  updateTodo() {
+    this.dbService.update(this.todo).then(() => {
+      let index = this.todos.findIndex((todo) => todo.id === this.todo.id);
       this.todos[index] = this.todo;
       this.clear();
     });
   }
-  check(todo: Todo) {
+
+  onTodoCompleted(todo: Todo) {
     this.todo = todo;
     this.todo.done = !todo.done;
-    this.update();
+    this.updateTodo();
   }
 
-  arrayRemove(arry: Todo[], id: string) {
-    return arry.filter((ele) => ele.id != id);
-  }
-  delete(todo: Todo) {
-    this.api
+  onDeleteTodo(todo: Todo) {
+    this.dbService
       .deleteTodo(todo)
-      .then((dado) => (this.todos = this.arrayRemove(this.todos, todo.id)));
+      .then(() => (this.todos = this.removeFromArray(this.todos, todo.id)));
+  }
+
+  // helper functions
+  removeFromArray(arry: Todo[], id: string) {
+    return arry.filter((ele) => ele.id != id);
   }
 }
